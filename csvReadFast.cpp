76fcecd4ba042e181f6cb4dc65bc4d8ca3015b31
chathura226 <chathuralakshan226@gmail.com>
2023-03-12 21:38:35 +0530

    first commit

---
+++ csvReadFast.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstdio>
+int main() {
+	int a, b, c;
+	if (FILE* filePointer = fopen("test.csv", "r")) {
+
+		while (fscanf(filePointer, "%d,%d,%d", &a, &b, &c) == 3) {
+			std::cout << a << " " << b << " " << c << std::endl;
+		}
+	}
+}
