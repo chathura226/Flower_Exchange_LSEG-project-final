5c8768d1fbc0c52480cd5dddf91b8ce8e96bd7b2
chathura226 <chathuralakshan226@gmail.com>
2023-03-16 22:16:24 +0530

    validation

--- main.cpp
+++ main.cpp
@@ -18,9 +18,29 @@ public:
     int qty;
     int priority;
     std::string status;
+    orderObj(std::string& cliOrd, std::string& inst, int &side,int &qty,double &price){
+        clientOrderID=cliOrd;
+        this->inst=inst;
+        this->side=side;
+        this->qty=qty;
+        this->price=price;
+        priority=1;
+        status="New";
 
+        if(cliOrd.size()==0 || inst.size()==0 )status="Rejected";
+        else if(inst!="Rose" &&  inst!="Lavender"&& inst!="Lotus"&& inst!="Tulip"&& inst!="Orchid")status="Rejected";
+        else if(price<=0 || qty<10 || qty>1000 || qty%10!=0)status="Rejected";
+        else if(side!=1 && side!=2)status="Rejected";
+
+    }
+    
 };
 void writeToFile(orderObj*,int,double);
+
+
+void initializeInstrumentArray();
+int initializeIns(int);
+int validateAndCreate(std::string &cliOrd, std::string &inst, int &side,int &qty,double &price);
 
 class instrument{
 public:
@@ -40,13 +60,11 @@ public:
     }
 };
 
-
-
+instrument* allInstruments[5];
 
 int main(){
+    initializeInstrumentArray();
     file.open("execution_rep.csv",std::ios_base::app);//to append
-    
-    instrument rose;
     char cliOrd[7],inst[8];
     int side,qty;
     double price;
@@ -54,16 +72,11 @@ int main(){
         std::cout<<"hi"<<std::endl;
 		while (fscanf(filePointer, "%[^,],%[^,],%d,%d,%lf", cliOrd, inst, &side,&qty,&price)==5) {
             std::cout<<cliOrd<<inst<<side<<qty<<price<<std::endl;
-            orderObj* o1=new orderObj;
-            o1->clientOrderID=cliOrd;
-            o1->inst=inst;
-            o1->side=side;
-            o1->price=price;
-            o1->qty=qty;
-            o1->priority=1;
-            o1->status="New";
-            rose.newOrder(o1);
-            delete o1;
+            std::string cliOrdString=std::string(cliOrd);
+            std::string instString=std::string(inst);
+            int temp=validateAndCreate(cliOrdString,instString,side,qty,price);
+            if(temp==1)continue;
+
 		}
         fclose(filePointer);
         
@@ -86,4 +99,44 @@ void writeToFile(orderObj* x,int qty,double price){
         <<x->status<<","
         <<qty<<","
         <<price;
+}
+
+
+void initializeInstrumentArray(){
+    allInstruments[0]=NULL;
+    allInstruments[1]=NULL;
+    allInstruments[2]=NULL;
+    allInstruments[3]=NULL;
+    allInstruments[4]=NULL;
+}
+
+int initializeIns(int i){
+    instrument *temp=new instrument;
+    if(temp==NULL){
+        std::cout<<"Error allocating memory for instrument book!\n";
+        return 1;
+    }
+    allInstruments[i]=temp;
+    return 0;
+
+}
+
+int validateAndCreate(std::string& cliOrd, std::string& inst, int &side,int &qty,double &price){
+    orderObj* temp=new orderObj(cliOrd, inst, side,qty,price);
+    if(temp->status=="Rejected"){
+        writeToFile(temp,temp->qty,temp->price);
+        delete(temp);
+        return 1;
+    }
+    int i;
+    if(temp->inst==std::string("Rose"))i=0;
+    else if(temp->inst==std::string("Lavender"))i=1;
+    else if(temp->inst==std::string("Lotus"))i=2;
+    else if(temp->inst==std::string("Tulip"))i=3;
+    else if(temp->inst==std::string("Orchid"))i=4;
+    
+    if(allInstruments[i]==NULL)initializeIns(i);
+    allInstruments[i]->newOrder(temp);
+    return 0;
+
 }
